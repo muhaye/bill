fb88046a4e61624b46046e635bb16d0f7b97b52c
Ngendo Muhayimana <muhaye@muh.mobi>
2017-11-08 19:42:15 -0500

    - add getopt

--- bill.c
+++ bill.c
@@ -4,6 +4,9 @@
 
 #include <stdio.h> 
 #include <string.h> 
+#include <unistd.h>
+#include <stdlib.h>    /* for exit */
+#include <getopt.h>
 
 #define MAX_LEN 80
 
@@ -11,36 +14,85 @@
 int addClient(); 
 
 // Implementation -- Section
-int main(int argc, char *argv[]) { 
-	
-	if ( argc > 2 ) {
-		printf("Please add valid parameter "); 
-		return -1;	
-	}
 
-	char *a_command = argv[1];
+int main(int argc, char **argv) {
+  int c;
+  int digit_optind = 0;
 
-	if ( strcmp(a_command, "addClient") == 0 ) {
+  while (1) {
+    int this_option_optind = optind ? optind : 1;
+    int option_index = 0;
+    static struct option long_options[] = {
+      {"add",     required_argument, 0,  0 },
+      {"append",  no_argument,       0,  0 },
+      {"delete",  required_argument, 0,  0 },
+      {"verbose", no_argument,       0,  0 },
+      {"create",  required_argument, 0, 'c'},
+      {"file",    required_argument, 0,  0 },
+      {0,         0,                 0,  0 }
+    };
 
-		return addClient();
+    c = getopt_long(argc, argv, "abc:d:012",
+        long_options, &option_index);
+    if (c == -1)
+      break;
 
-	} else if (strcmp(a_command, "addBill") == 0 ) {
+    switch (c) {
+      case 0:
+        printf("option %s", long_options[option_index].name);
+        if (optarg)
+          printf(" with arg %s", optarg);
+        printf("\n");
+        break;
 
-		return addClient();
+      case '0':
+      case '1':
+      case '2':
+        if (digit_optind != 0 && digit_optind != this_option_optind)
+          printf("digits occur in two different argv-elements.\n");
+        digit_optind = this_option_optind;
+        printf("option %c\n", c);
+        break;
 
-	} else {
-		// Unreconized parameter
-		printf("Unreconized parameter: %s\n", a_command); 
-	}
+      case 'a':
+        printf("option a\n");
+        break;
 
-	return -1;
+      case 'b':
+        printf("option b\n");
+        break;
+
+      case 'c':
+        printf("option c with value '%s'\n", optarg);
+        break;
+
+      case 'd':
+        printf("option d with value '%s'\n", optarg);
+        break;
+
+      case '?':
+        break;
+
+      default:
+        printf("?? getopt returned character code 0%o ??\n", c);
+    }
+  }
+
+  if (optind < argc) {
+    printf("non-option ARGV-elements: ");
+    while (optind < argc)
+      printf("%s ", argv[optind++]);
+    printf("\n");
+  }
+
+  exit(EXIT_SUCCESS);
 }
 
 int addClient() {
-	
-	char a_client[MAX_LEN];
-	printf("Please enter client Name ex:(Candy inc.):\n"); 
-	scanf("%99[^\n]", a_client);
-	printf("You entered: %s\n", a_client); 
-	return 0; 
+
+  char a_client[MAX_LEN];
+  printf("Please enter client Name ex:(Candy inc.):\n"); 
+  scanf("%99[^\n]", a_client);
+  printf("You entered: %s\n", a_client); 
+  return 0; 
 }
